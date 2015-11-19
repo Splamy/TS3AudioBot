83a42a396d2632577c9de2b51bf9bee06bdeac0a
Splamy <splamyn@gmail.com>
2015-11-19 01:51:37 +0100

    Added MulTrie for future use in the HistoryManager
    -MulTrie needs some unit tests
    -fixed some typos

--- TS3AudioBot/Helper/Trie.cs
+++ TS3AudioBot/Helper/Trie.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.Text;
+using System.Collections.Generic;
 
 namespace TS3AudioBot.Helper
 {
 	public class Trie<T> where T : class
 	{
-		const int CHARS_IN_ALPHABET = 26;
-		TrieNode root;
+		protected const int CHARS_IN_ALPHABET = 26;
+		protected TrieNode root;
 
 		public Trie()
 		{
-			root = new TrieNode(null, '+', null);
+			root = GetNewTrieNode(null, '+');
 			root.unique = false;
 		}
 
-		public void Add(string key, T value)
+		public virtual void Add(string key, T value)
 		{
 			TrieNode current = root;
 			foreach (char c in key)
@@ -27,40 +28,57 @@ namespace TS3AudioBot.Helper
 				else
 				{
 					if (!current.hasData)
-						current.data = null;
+						current.Data = null;
 					current.unique = false;
 				}
 				TrieNode child = current.children[index];
 				if (child == null)
 				{
-					child = new TrieNode(current, c, value);
+					child = GetNewTrieNode(current, c);
+					child.Data = value;
 					current.children[index] = child;
 				}
 				current = child;
 			}
-			current.data = value;
+			current.Data = value;
 			current.hasData = true;
 		}
 
-		public bool TryGetValue(string key, out T value)
+		protected virtual TrieNode GetNewTrieNode(TrieNode parent, char index)
+		{
+			return new TrieNode(parent, index);
+		}
+
+		public virtual bool TryGetValue(string key, out T value)
+		{
+			TrieNode node = TryGetNode(key);
+			if (node != null && (node.hasData || node.unique))
+			{
+				value = node.Data;
+				return true;
+			}
+			else
+			{
+				value = null;
+				return false;
+			}
+		}
+
+		protected TrieNode TryGetNode(string key)
 		{
 			key = key.ToLower();
-            TrieNode current = root;
+			TrieNode current = root;
 			foreach (char c in key)
 			{
 				int index = ValidateChar(c);
 				if (index == -1 || current.children == null || (current = current.children[index]) == null)
-				{
-					value = null;
-					return false;
-				}
+					return null;
 				if (current.unique) break;
 			}
-			value = current.data;
-			return current.hasData || current.unique;
+			return current;
 		}
 
-		private static int ValidateChar(char c)
+		protected static int ValidateChar(char c)
 		{
 			if (c >= 'a' && c <= 'z')
 				return c - 'a';
@@ -74,13 +92,13 @@ namespace TS3AudioBot.Helper
 			return strb.ToString();
 		}
 
-		private void ToStringGen(TrieNode tn, StringBuilder strb)
+		protected void ToStringGen(TrieNode tn, StringBuilder strb)
 		{
 			strb.Append(tn.charId);
 			if (tn.hasData)
 			{
 				strb.Append("[");
-				strb.Append(tn.data);
+				strb.Append(tn.Data);
 				strb.Append("]");
 			}
 			else if (tn.unique)
@@ -99,23 +117,113 @@ namespace TS3AudioBot.Helper
 			}
 		}
 
-		private class TrieNode
+		protected class TrieNode
 		{
-			public T data;
-			public TrieNode parent;
+			public T Data { get; set; }
+			readonly public TrieNode parent;
 			public TrieNode[] children;
-			public char charId;
+			readonly public char charId;
 			public bool hasData;
 			public bool unique;
 
-			public TrieNode(TrieNode trieNode, char charId, T data)
+			public TrieNode(TrieNode parent, char charId)
 			{
 				this.charId = charId;
-				this.data = data;
-				this.parent = trieNode;
+				this.parent = parent;
+				Data = null;
 				hasData = false;
 				unique = true;
 				children = null;
+			}
+		}
+	}
+
+	public class MulTrie<T> : Trie<T> where T : class
+	{
+		public override void Add(string key, T value)
+		{
+			TrieNode node = TryGetNode(key);
+			if (node != null && node.hasData) // key exists already
+				return; // maby update key
+
+			MulNode mulRoot = (MulNode)root;
+
+			HashSet<MulNode> useNodes = new HashSet<MulNode>();
+			//useNodes.Add(mulRoot);
+			for (int i = 0; i < key.Length; i++)
+			{
+				string subKey = key.Substring(i);
+				MulNode current = mulRoot;
+				foreach (char c in subKey)
+				{
+					int index = ValidateChar(c);
+					if (current.children == null)
+					{
+						current.children = new TrieNode[CHARS_IN_ALPHABET];
+					}
+					MulNode child = (MulNode)current.children[index];
+					if (child == null)
+					{
+						child = (MulNode)GetNewTrieNode(current, c);
+						current.children[index] = child;
+						useNodes.Add(child);
+					}
+					else
+					{
+						if (!useNodes.Contains(child))
+							useNodes.Add(child);
+					}
+					current = child;
+				}
+				if (i == 0)
+				{
+					current.Data = value;
+					current.hasData = true;
+				}
+			}
+			foreach (var useNode in useNodes)
+			{
+				useNode.subReferences.Add(value);
+			}
+		}
+
+		protected override TrieNode GetNewTrieNode(TrieNode parent, char index)
+		{
+			return new MulNode(parent, index);
+		}
+
+		public override bool TryGetValue(string key, out T value)
+		{
+			MulNode node = (MulNode)TryGetNode(key);
+			if (node != null)
+			{
+				value = node.Data;
+				return true;
+			}
+			else
+			{
+				value = null;
+				return false;
+			}
+		}
+
+		public IList<T> GetValues(string key)
+		{
+			MulNode node = (MulNode)TryGetNode(key);
+			if (node != null)
+				return node.subReferences.AsReadOnly();
+			else
+				return null;
+		}
+
+		protected class MulNode : TrieNode
+		{
+			public List<T> subReferences;
+
+			public MulNode(TrieNode parent, char index) : base(parent, index)
+			{
+				subReferences = new List<T>();
+				unique = false;
 			}
 		}
 	}
--- TS3AudioBot/MainBot.cs
+++ TS3AudioBot/MainBot.cs
@@ -172,20 +172,20 @@ namespace TS3AudioBot
 
 			builder.New("add").Action(CommandAdd).Permission(CommandRights.Private).HelpData("Adds a new song to the queue.", "<link>").Finish();
 			builder.New("clear").Action(CommandClear).Permission(CommandRights.Private).HelpData("Removes all songs from the current playlist.").Finish();
-			builder.New("getuserid").Action(CommandGetUserId).Permission(CommandRights.Private).HelpData("Gets the unique Id of a user.", "<username>").Finish();
+			builder.New("getuserid").Action(CommandGetUserId).Permission(CommandRights.Admin).HelpData("Gets the unique Id of a user.", "<username>").Finish();
 			builder.New("help").Action(CommandHelp).Permission(CommandRights.Private).HelpData("Shows all commands or detailed help about a specific command.", "[<command>]").Finish();
 			builder.New("history").Action(CommandHistory).Permission(CommandRights.Private).HelpData("Shows recently played songs.").Finish();
 			builder.New("kickme").Action(CommandKickme).Permission(CommandRights.Private).HelpData("Guess what?", "[far]").Finish();
 			builder.New("link").Action(CommandLink).Permission(CommandRights.Private).HelpData("Plays any direct ressource link.", "<link>").Finish();
-			builder.New("loop").Action(CommandLoop).Permission(CommandRights.Private).HelpData("Sets wether of not to loop the entire playlist.", "(on|off)").Finish();
+			builder.New("loop").Action(CommandLoop).Permission(CommandRights.Private).HelpData("Sets whether or not to loop the entire playlist.", "(on|off)").Finish();
 			builder.New("next").Action(CommandNext).Permission(CommandRights.Private).HelpData("Plays the next song in the playlist.").Finish();
-			builder.New("pm").Action(CommandPM).Permission(CommandRights.Public).HelpData("Reuests private session with the ServerBot so you can invoke private commands.").Finish();
+			builder.New("pm").Action(CommandPM).Permission(CommandRights.Public).HelpData("Requests a private session with the ServerBot so you can invoke private commands.").Finish();
 			builder.New("play").Action(CommandPlay).Permission(CommandRights.Private)
-				.HelpData("Automatically tries to decide wether the link is a special ressource (like youtube) or a direct ressource (like ./hello.mp3) and starts it", "<link>").Finish();
+				.HelpData("Automatically tries to decide whether the link is a special ressource (like youtube) or a direct ressource (like ./hello.mp3) and starts it", "<link>").Finish();
 			builder.New("previous").Action(CommandPrevious).Permission(CommandRights.Private).HelpData("Plays the previous song in the playlist.").Finish();
 			builder.New("quit").Action(CommandQuit).Permission(CommandRights.Admin).HelpData("Closes the TS3AudioBot application.").Finish();
 			builder.New("quiz").Action(CommandQuiz).Permission(CommandRights.Public).HelpData("Enable to hide the songnames and let your friends guess the title.", "(on|off)").Finish();
-			builder.New("repeat").Action(CommandRepeat).Permission(CommandRights.Private).HelpData("Sets wether or not to loop a single song", "(on|off)").Finish();
+			builder.New("repeat").Action(CommandRepeat).Permission(CommandRights.Private).HelpData("Sets whether or not to loop a single song", "(on|off)").Finish();
 			builder.New("seek").Action(CommandSeek).Permission(CommandRights.Private).HelpData("Jumps to a timemark within the current song.", "(<time in seconds>|<seconds>:<minutes>)").Finish();
 			builder.New("song").Action(CommandSong).Permission(CommandRights.AnyVisibility).HelpData("Tells you the name of the current song.").Finish();
 			builder.New("soundcloud").Action(CommandSoundcloud).Permission(CommandRights.Private).HelpData("Resolves the link as a soundcloud song to play it for you.").Finish();
@@ -477,6 +477,12 @@ namespace TS3AudioBot
 
 		private void CommandQuiz(BotSession session, string parameter)
 		{
+			if (session.IsPrivate)
+			{
+				session.Write("No cheatig! Everybody has to see it!");
+				return;
+			}
+
 			if (parameter == "on")
 				QuizMode = true;
 			else if (parameter == "off")
