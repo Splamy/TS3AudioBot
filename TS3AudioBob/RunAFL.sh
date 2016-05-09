f415eb2ea412fda37d87323bc2e54008cabf228b
Flakebi <flakebi@t-online.de>
2016-05-09 17:27:51 +0200

    Display parts of the fancy afl output

--- TS3AudioBob/RunAFL.sh
+++ TS3AudioBob/RunAFL.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export AFL_SKIP_CPUFREQ=1
 
-afl-fuzz -i testcases -o findings -m 500M -M fuzzer00 -- bin/ts3audiobobfuzz > /dev/null &
+afl-fuzz -i testcases -o findings -m 500M -M fuzzer00 -- bin/ts3audiobobfuzz &
 for i in `seq \`nproc\``; do
 	afl-fuzz -i testcases -o findings -m 500M -S fuzzer0$i -- bin/ts3audiobobfuzz > /dev/null &
 done
