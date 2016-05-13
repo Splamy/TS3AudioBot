dcf549c31b03bcedcfbced6789c2bc8deeeec5a9
Flakebi <flakebi@t-online.de>
2016-05-14 01:54:58 +0200

    Build with clang again

--- TS3AudioBob/RunAFL.sh
+++ TS3AudioBob/RunAFL.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+folder=bin
+
 export AFL_SKIP_CPUFREQ=1
 
-afl-fuzz -i testcases -o findings -m 500M -M fuzzer00 -- bin/ts3audiobobfuzz &
+afl-fuzz -i testcases -o findings -m 500M -M fuzzer00 -- "$folder/ts3audiobobfuzz" &
 for i in `seq \`nproc\``; do
-	afl-fuzz -i testcases -o findings -m 500M -S fuzzer0$i -- bin/ts3audiobobfuzz > /dev/null &
+	afl-fuzz -i testcases -o findings -m 500M -S fuzzer0$i -- "$folder/ts3audiobobfuzz" > /dev/null &
 done
 
 echo 'Call `afl-whatsup findings` to see the current status'
