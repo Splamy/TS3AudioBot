961622989cbca7a48ad0044fa2c8017c400756a6
Splamy <splamyn@gmail.com>
2020-03-16 02:19:19 +0100

    Fixed tests

--- TS3ABotUnitTests/UnitTests.cs
+++ TS3ABotUnitTests/UnitTests.cs
@@ -224,7 +224,7 @@ namespace TS3ABotUnitTests
 		public void Factory_YoutubeFactoryTest()
 		{
 			var ctx = new ResolveContext(null, null);
-			using (IResourceResolver rfac = new YoutubeResolver(null))
+			using (IResourceResolver rfac = new YoutubeResolver(new ConfFactories()))
 			{
 				// matching links
 				Assert.AreEqual(rfac.MatchResource(ctx, @"https://www.youtube.com/watch?v=robqdGEhQWo"), MatchCertainty.Always);
