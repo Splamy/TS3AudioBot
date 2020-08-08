9d05782f2c795a6b1e8d3d78eb4e6aa1b3e52783
Splamy <splamyn@gmail.com>
2020-08-08 03:33:15 +0200

    Updating npm pkgs

--- WebInterface/webpack.config.js
+++ WebInterface/webpack.config.js
@@ -4,81 +4,83 @@ const CopyPlugin = require('copy-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-    entry: './src/ts/Main.ts',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: [{
-                    loader: 'ts-loader',
-                    options: {
-                        appendTsSuffixTo: [/\.vue$/]
-                    }
-                }],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'less-loader',
-                ],
-            },
-            {
-                test: /\.svg$/,
-                loader: 'svg-inline-loader'
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg|eot|woff|ttf|svg|woff2)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: "[path][name].[ext]"
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js', '.vue'],
-        alias: {
-            'vue$': 'vue/dist/vue.esm.js'
-        }
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'TS3AudioBot',
-            template: 'src/html/index.html'
-        }),
-        new CopyPlugin([
-            { from: 'src/html', to: '.' },
-        ]),
-        new VueLoaderPlugin()
-    ],
-    devServer: {
-        disableHostCheck: true,
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET",
-            "Access-Control-Allow-Headers": "X-Requested-With, Content-Type, Authorization"
-        }
-    }
+	entry: './src/ts/Main.ts',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/i,
+				use: [{
+					loader: 'ts-loader',
+					options: {
+						appendTsSuffixTo: [/\.vue$/]
+					}
+				}],
+				exclude: /node_modules/
+			},
+			{
+				test: /\.less$/i,
+				use: [
+					'style-loader',
+					'css-loader',
+					'less-loader',
+				],
+			},
+			{
+				test: /\.svg$/i,
+				loader: 'svg-inline-loader'
+			},
+			{
+				test: /\.vue$/i,
+				loader: 'vue-loader'
+			},
+			{
+				test: /\.css$/i,
+				use: [
+					'vue-style-loader',
+					'css-loader',
+				],
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg|eot|woff|ttf|svg|woff2)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: "[path][name].[ext]"
+						}
+					}
+				]
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js', '.vue'],
+		alias: {
+			'vue$': 'vue/dist/vue.esm.js'
+		}
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'TS3AudioBot',
+			template: 'src/html/index.html'
+		}),
+		new CopyPlugin({
+			patterns: [
+				{ from: 'src/html', to: '.' },
+			]
+		}),
+		new VueLoaderPlugin()
+	],
+	devServer: {
+		disableHostCheck: true,
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "GET",
+			"Access-Control-Allow-Headers": "X-Requested-With, Content-Type, Authorization"
+		}
+	}
 };
