180039a34e17ceb20735eb6a3529f7351dc55a66
Splamy <splamyn@gmail.com>
2022-01-12 13:51:51 +0100

    Fixed nullable warning is sourcegen

--- TSLibAutogen/Util.cs
+++ TSLibAutogen/Util.cs
@@ -34,6 +34,16 @@ using ConnectionId = System.UInt32;
 using EccKeyPubP256 = TSLib.Uid;
 ";
 
+	public const string LicenseHeader =
+@"// TSLib - A free TeamSpeak 3 and 5 client library
+// Copyright (C) 2017  TSLib contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the Open Software License v. 3.0
+//
+// You should have received a copy of the Open Software License along with this
+// program. If not, see <https://opensource.org/licenses/OSL-3.0>.";
+
 
 #pragma warning disable RS2008 // Enable analyzer release tracking
 	public static readonly DiagnosticDescriptor ParseErrorDiag = new("TSDECL_1", "Parse Error", "Parse errorn in module {0}", "TSLib.Autogen", DiagnosticSeverity.Error, true);
